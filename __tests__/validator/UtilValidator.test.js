641b30ddc73093d9d87f2f4ed8703870b655d06f
iftype <iftype@naver.com>
2025-10-30 04:32:07 +0900

    test(validator): 🔴 UtilValidator isInteger 테스트 추가
    - 입력한 값이 정수인지 테스트 성공/실패 케이스 추가

--- __tests__/validator/UtilValidator.test.js
+++ __tests__/validator/UtilValidator.test.js
@@ -1,6 +1,28 @@
 import UtilValidator from '../../src/validator/UtilValidator.js';
 
 describe('UtilValidator', () => {
+  describe('정수인지 검사 isInteger', () => {
+    test.each([
+      [1, true],
+      [0, true],
+      [-1, true],
+      ['0', true],
+      ['1', true],
+    ])('⭕성공 테스트 (%s) return %s', (test, expected) => {
+      expect(UtilValidator.isInteger(test)).toBe(expected);
+    });
+    test.each([
+      ['', false],
+      [' ', false],
+      [undefined, false],
+      [null, false],
+      [0.1, false],
+      ['str', false],
+    ])('❌실패 테스트(%s) return %s', (test, expected) => {
+      expect(UtilValidator.isInteger(test)).toBe(expected);
+    });
+  });
+
   describe('양수인지 검사 isPositive', () => {
     test.each([
       [1, true],
