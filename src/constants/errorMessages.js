037eafbd12f86d9c421e1ff22380a0c56bb04959
iftype <iftype@naver.com>
2025-10-30 06:09:23 +0900

    chore(errorMessages): 에러메세지 추가
    - LOTTO_DUPLICATE: 중복검사
    - LOTTO_QUANTITY: 수량검사
    - LOTTO_RANGE: 범위검사

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -2,9 +2,13 @@ const PREFIX = '[ERROR]';
 
 const ERROR_MESSAGES = Object.freeze({
   FORMAT_NOT_NUM: `${PREFIX}구매 금액 형식이 잘못되었습니다`,
-  UNIT: `${PREFIX}구매 금액은 1,000원 단위로 입력해야됩니다`,
   POSITVE: `${PREFIX}입력 값이 양수여야 합니다`,
   INTEGER: `${PREFIX}입력 값이 정수여야 합니다`,
+
+  LOTTO_UNIT: `${PREFIX}구매 금액은 1,000원 단위로 입력해야됩니다`,
+  LOTTO_DUPLICATE: `${PREFIX}로또 번호가 중복됐습니다`,
+  LOTTO_QUANTITY: `${PREFIX}로또 번호는 6개여야합니다`,
+  LOTTO_RANGE: `${PREFIX}로또 번호는 1-45여야 합니다`,
 });
 
 export default ERROR_MESSAGES;
