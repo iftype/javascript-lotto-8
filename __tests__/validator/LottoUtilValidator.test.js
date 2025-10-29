3a9a8157a969a8f6d8801ba2e449dc14137d7b30
iftype <iftype@naver.com>
2025-10-30 07:21:50 +0900

    test(LottoUtilValidator): 🔴LottoUtilValidator 테스트 추가
    - isNumber: 각 요소들이 숫자가 맞는지 테스트

--- __tests__/validator/LottoUtilValidator.test.js
+++ __tests__/validator/LottoUtilValidator.test.js
@@ -1,9 +1,26 @@
 import LottoUtilValidator from '../../src/validator/LottoUtilValidator.js';
+// 🔴 전부 숫자인가?
 // 🔴 번호는 6개인가?
 // 🔴 숫자가 1-45인가?
 // 🔴 로또 숫자가 중복은 없는가?
 // 🔴 해당 번호가 로또 리스트에 포함되어있나?
 describe('LottoUtilValidator', () => {
+  describe('요소들이 숫자가 맞는지', () => {
+    test.each([[[1, 2, 3, 4, 5, 6], true]])(
+      '⭕성공 테스트(%s) throw Error %s',
+      (test, expected) => {
+        expect(LottoUtilValidator.isNumber(test)).toBe(expected);
+      },
+    );
+    test.each([
+      [['1, 2, 3, 4, 5,6'], false],
+      [[null, 2, 3, 4, 5, 6, 7], false],
+      [[undefined, 2, 3, 4, 5, 6, 7], false],
+    ])('❌실패 테스트(%s) throw Error %s', (test, expected) => {
+      expect(LottoUtilValidator.isNumber(test)).toBe(expected);
+    });
+  });
+
   describe('6개가 맞는지 isQuantity', () => {
     test.each([[[1, 2, 3, 4, 5, 6], true]])(
       '⭕성공 테스트(%s) throw Error %s',
