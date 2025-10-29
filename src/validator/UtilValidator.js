9c2ed87f09bcffd3ed18eb67af36bdcfc7716baa
iftype <iftype@naver.com>
2025-10-29 17:04:34 +0900

    feat(validator): 🟢 UtilValidator.isConvertNum 함수 구현
    - 입력한 값에 대해서 숫자로 변환 가능한지
    - null 과 undefined 인지 체크
    - 문자열로 변환하여 공백인지 체크
    - 숫자로 변환해보고 체크

---
+++ src/validator/UtilValidator.js
@@ -0,0 +1,16 @@
+const UtilValidator = {
+  isConvertNum(param) {
+    if (param === null || typeof param === 'undefined') {
+      return false;
+    }
+    if (String(param).trim() === '') {
+      return false;
+    }
+    if (Number.isNaN(Number(param))) {
+      return false;
+    }
+    return true;
+  },
+};
+
+export default UtilValidator;
