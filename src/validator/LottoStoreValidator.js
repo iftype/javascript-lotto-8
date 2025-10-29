fb1a312d3906a294965242f0b6a6fa05274ad3ab
iftype <iftype@naver.com>
2025-10-30 05:32:44 +0900

    feat(validator): 🟢LottoStoreValidator validate 추가
    - 타입,정수,양수,단위 순서로 진행

---
+++ src/validator/LottoStoreValidator.js
@@ -0,0 +1,21 @@
+import ERROR_MESSAGES from '../constants/errorMessages.js';
+import UtilValidator from './UtilValidator.js';
+
+class LottoStoreValidator {
+  static validate(purchase) {
+    if (!UtilValidator.isConvertNum(purchase)) {
+      throw new Error(ERROR_MESSAGES.FORMAT_NOT_NUM);
+    }
+    if (!UtilValidator.isInteger(purchase)) {
+      throw new Error(ERROR_MESSAGES.INTEGER);
+    }
+    if (!UtilValidator.isPositive(purchase)) {
+      throw new Error(ERROR_MESSAGES.POSITVE);
+    }
+    if (Number(purchase) % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGES.UNIT);
+    }
+  }
+}
+
+export default LottoStoreValidator;
