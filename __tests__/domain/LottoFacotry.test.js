e6310f2c90dea50700b579ac1c4980057298beb4
iftype <iftype@naver.com>
2025-10-30 12:28:56 +0900

    test(Lotto): 🔴LottoFactory 클래스의 생성 테스트 추가
    - Lotto의 type으로 이루어진 lottos 배열이 반환됨

---
+++ __tests__/domain/LottoFacotry.test.js
@@ -0,0 +1,12 @@
+import Lotto from '../../src/domain/Lotto.js';
+
+describe('LottoFactory', () => {
+  test('create 구매하면 Lotto List반환', () => {
+    const purchaseAmount = 10000;
+    const lottos = LottoFactory.create(purchaseAmount);
+    expect(lottos).toHaveLength(10);
+    lottos.forEach((lotto) => {
+      expect(lotto).toBeInstanceOf(Lotto);
+    });
+  });
+});
