09cfdf0308ad5bfa268e55dc8009461bee8d0ab8
iftype <iftype@naver.com>
2025-10-31 18:21:29 +0900

    refactor(errorMessages): 에러 메세지 상수 내용 변경
    - LOTTO_QUANTITY: 특정 수량이 아닌 포괄적인 정보로 변경
    - FORMAT_NOT_NUM: 구매금액에 대한 정보에서 포괄적인 정보로 변경

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,15 +1,15 @@
 const PREFIX = '[ERROR]';
 
 const ERROR_MESSAGES = Object.freeze({
-  FORMAT_NOT_NUM: `${PREFIX}구매 금액 형식이 잘못되었습니다`,
   POSITVE: `${PREFIX}입력 값이 양수여야 합니다`,
   INTEGER: `${PREFIX}입력 값이 정수여야 합니다`,
 
+  FORMAT_NOT_NUM: `${PREFIX} 숫자를 입력해야 합니다`,
   PURCHASE_UNIT: `${PREFIX}구매 금액은 1,000원 단위로 입력해야됩니다`,
 
   LOTTO_NOT_NUMBER: `${PREFIX}로또 번호는 숫자만 추가 할 수 있습니다`,
   LOTTO_DUPLICATE: `${PREFIX}로또 번호가 중복됐습니다`,
-  LOTTO_QUANTITY: `${PREFIX}로또 번호는 6개여야합니다`,
+  LOTTO_QUANTITY: `${PREFIX}로또 번호는 정의된 만큼 생성되어야 합니다 `,
   LOTTO_RANGE: `${PREFIX}로또 번호는 1-45여야 합니다`,
 });
 
