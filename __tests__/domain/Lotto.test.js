69e2b2ac2ced38286855caee3d3db52ab15ea3fd
iftype <iftype@naver.com>
2025-10-31 13:35:31 +0900

    feat(Lotto): Lotto 클래스 구현 및 테스트 추가
     - lottoNumber의 인스턴스 배열을 프로퍼티로 가짐
     - Number은 Map 객체이기 때문에 n.getNumber()등으로 접근
     - validator: 수량과 중복 검사를함

---
+++ __tests__/domain/Lotto.test.js
@@ -0,0 +1,35 @@
+import Lotto from '../../src/domain/Lotto.js';
+import LottoNumber from '../../src/domain/LottoNumber.js';
+import ERROR_MESSAGES from '../../src/constants/errorMessages.js';
+
+const createlottoNumbers = (numbers) => {
+  return numbers.map((numbers) => new LottoNumber(numbers));
+};
+
+describe('로또 클래스 테스트', () => {
+  describe('생성 유효성 테스트', () => {
+    test.each([
+      [[1, 2, 3, 4, 5], ERROR_MESSAGES.LOTTO_QUANTITY],
+      [[1, 2, 3, 4, 5, 6, 7], ERROR_MESSAGES.LOTTO_QUANTITY],
+    ])('❌수량 에러 테스트(%s) throw Error %s', (numbers, errorMessage) => {
+      expect(() => new Lotto(createlottoNumbers(numbers))).toThrow(errorMessage);
+    }); // 실패테스트 수량
+
+    test.each([[[1, 1, 3, 4, 5, 6], ERROR_MESSAGES.LOTTO_DUPLICATE]])(
+      '❌중복 에러 테스트(%s) throw Error %s',
+      (numbers, errorMessage) => {
+        expect(() => new Lotto(createlottoNumbers(numbers))).toThrow(errorMessage);
+      },
+    ); // 실패테스트 중복됐는가
+  });
+
+  describe('메서드 테스트', () => {
+    test('⭕메서드 테스트() throw Error ', () => {
+      const numbers = [6, 2, 3, 4, 5, 1];
+      const lotto = new Lotto(createlottoNumbers(numbers));
+
+      const resultArray = [1, 2, 3, 4, 5, 6];
+      expect(lotto.getNumbers()).toEqual(resultArray);
+    }); // 성공 테스트
+  });
+});
---
+++ src/domain/Lotto.js
@@ -0,0 +1,39 @@
+import ERROR_MESSAGES from '../constants/errorMessages';
+import LOTTO_SETTING from '../constants/lottoSetting';
+
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = this.#sort(numbers);
+  }
+
+  #validate(numbers) {
+    if (!this.#isQuantity(numbers)) {
+      throw new Error(ERROR_MESSAGES.LOTTO_QUANTITY);
+    }
+    if (!this.#isDuplicate(numbers)) {
+      throw new Error(ERROR_MESSAGES.LOTTO_DUPLICATE);
+    }
+  }
+
+  #sort(numbers) {
+    return [...numbers].sort((a, b) => a.getNumber() - b.getNumber());
+  }
+
+  #isQuantity(numbers) {
+    return numbers.length === LOTTO_SETTING.MAX_QUANTITY;
+  }
+
+  #isDuplicate(numbers) {
+    const newLotto = numbers.map((number) => number.getNumber());
+    const setLotto = new Set(newLotto);
+    return setLotto.size === newLotto.length;
+  }
+
+  getNumbers() {
+    return [...this.#numbers].map((number) => number.getNumber());
+  }
+}
+export default Lotto;
