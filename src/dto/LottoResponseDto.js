a4bf0c1ba4cdabd89a6946cb5b3a37dd46717843
iftype <iftype@naver.com>
2025-11-01 19:22:16 +0900

    feat(LottoResponseDto): LottoResponseDto클래스 추가
    -  서비스에서 컨트롤러로 데이터 이동을 위해 구현
    - 컨트롤러는  Lottos의 값을 배열상태로 확인가능하다

---
+++ src/dto/LottoResponseDto.js
@@ -0,0 +1,15 @@
+class LottoResponseDto {
+  #lottos;
+
+  constructor({ lottos }) {
+    this.#lottos = lottos;
+  }
+
+  toJSON() {
+    const lottosToArray = [...this.#lottos].map((lotto) => lotto.getNumbers());
+    return {
+      lottos: lottosToArray,
+    };
+  }
+}
+export default LottoResponseDto;
