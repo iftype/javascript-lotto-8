1fdf8b9fe18459877421eecc4ada56a477ba4484
iftype <iftype@naver.com>
2025-11-01 10:21:51 +0900

    refactor(LottoStore): LottoState추가에 따른 프로퍼티 삭제
    - 구매 금액 프로퍼티 삭제

--- src/domain/LottoStore.js
+++ src/domain/LottoStore.js
@@ -1,14 +1,11 @@
 class LottoStore {
-  #purchaseAmount = 0;
   #lottofactory;
 
   constructor(lottofactory) {
     this.#lottofactory = lottofactory;
   }
 
-  buyLotto(purchaseAmount, count) {
-    this.#purchaseAmount = purchaseAmount;
-
+  buyLotto(count) {
     const lottos = [];
     for (let i = 0; i < count; i += 1) {
       lottos.push(this.#lottofactory.createLotto());
