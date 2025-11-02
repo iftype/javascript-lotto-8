75075a4add29c9acaac1fab4279ab5f909b3ee41
iftype <iftype@naver.com>
2025-11-02 09:56:39 +0900

    fix(BonusNumberValidator): 유효성 검사 에러 수정
    - 보너스번호의 값을 받아와서 검증하는 것으로 수정
    - 확실한 구분을 위해 변수이름 수정 numbers->LottoNubmers

--- src/validator/domain/BonusNumberValidator.js
+++ src/validator/domain/BonusNumberValidator.js
@@ -2,22 +2,25 @@ import ERROR_MESSAGES from '../../constants/errorMessages.js';
 import LottoNumber from '../../domain/LottoNumber.js';
 
 class BonusNumberValidator {
-  static validate(winningNumbers, bonusNumber) {
-    if (!BonusNumberValidator.#isInRange(bonusNumber)) {
+  static validate(winningLottoNumbers, bonusLottoNumber) {
+    if (!BonusNumberValidator.#isInRange(bonusLottoNumber)) {
       throw new Error(ERROR_MESSAGES.LOTTO_RANGE);
     }
-    if (!BonusNumberValidator.#isDuplicate(winningNumbers, bonusNumber)) {
+    if (!BonusNumberValidator.#isDuplicate(winningLottoNumbers, bonusLottoNumber)) {
       throw new Error(ERROR_MESSAGES.LOTTO_DUPLICATE);
     }
   }
 
-  static #isInRange(bonusNumber) {
+  static #isInRange(bonusLottoNumber) {
+    const bonusNumber = bonusLottoNumber.getNumber();
     const [MIN_RANGE, MAX_RANGE] = Object.values(LottoNumber.getRange());
     return MIN_RANGE <= bonusNumber && bonusNumber <= MAX_RANGE;
   }
 
-  static #isDuplicate(winningNumbers, bonusNumber) {
-    return winningNumbers.includes(bonusNumber);
+  static #isDuplicate(winningLottoNumbers, bonusLottoNumber) {
+    console.log(winningLottoNumbers, bonusLottoNumber);
+    console.log(winningLottoNumbers.includes(bonusLottoNumber));
+    return !winningLottoNumbers.includes(bonusLottoNumber);
   }
 }
 
