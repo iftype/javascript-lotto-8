53338770bcf3bc4cf4acdf3be0907b23d593b904
iftype <iftype@naver.com>
2025-11-02 19:38:36 +0900

    test(LottoWinningResult): 테스트 추가
    - 1등과 2등의 결과값 테스트
    - 수익률 테스트 추가

---
+++ __tests__/domains/LottoWinningResult.test.js
@@ -0,0 +1,51 @@
+import LOTTO_SETTING from '../../src/constants/lottoSetting.js';
+import LottoWinningResult from '../../src/domains/LottoWinningResult.js';
+
+class MockLotto {
+  constructor(numbers) {
+    this.numbers = numbers;
+  }
+  hasLottoNumber(number) {
+    return this.numbers.includes(number);
+  }
+  countNumbers(numbers) {
+    return numbers.filter((num) => this.numbers.includes(num)).length;
+  }
+}
+describe('LottoWinningFactory 테스트', () => {
+  describe('createWinningLotto(numbers) 테스트', () => {
+    const resulFst = { FIRST: 1, SECOND: 0, THIRD: 0, FOURTH: 0, FIFTH: 0, OTHER: 0 };
+    const resulSec = { FIRST: 0, SECOND: 1, THIRD: 0, FOURTH: 0, FIFTH: 0, OTHER: 0 };
+    test.each([
+      [
+        [new MockLotto([1, 2, 3, 4, 5, 6])],
+        [1, 2, 3, 4, 5, 6],
+        7,
+        { winningStats: resulFst, totalWinningAmount: LOTTO_SETTING.PRIZES.FIRST },
+        ,
+      ],
+      [
+        [new MockLotto([1, 2, 3, 4, 5, 7])],
+        [1, 2, 3, 4, 5, 6],
+        7,
+        { winningStats: resulSec, totalWinningAmount: LOTTO_SETTING.PRIZES.SECOND },
+        ,
+      ],
+    ])(
+      'getWinningStats(lottos, winnintLottos, bonusLotto) 테스트 %s throw Error %s',
+      (lottos, winnintLottos, bonusLotto, Object) => {
+        expect(LottoWinningResult.getWinningStats(lottos, winnintLottos, bonusLotto)).toEqual(
+          Object,
+        );
+      },
+    ); // 성공 테스트
+  }); // 생성 테스트
+  describe('getWinningRate(purchaseAmount, totalWinningAmount)테스트', () => {
+    test.each([
+      [8000, 5000, '62.5'],
+      [12000, 7000, '58.3'],
+    ])('getWinningRate(%s, %s) 반환 값 %s', (purchaseAmount, totalWinningAmount, result) => {
+      expect(LottoWinningResult.getWinningRate(purchaseAmount, totalWinningAmount)).toBe(result);
+    }); // 실패테스트
+  }); // 생성 테스트
+}); // 설명
